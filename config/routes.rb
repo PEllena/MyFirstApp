7f3e4160e6ccb491d23a6eb0bbd6834293ef30f8
Elena Panfilova <ellena.panfilova@gmail.com>
2017-02-01 22:19:30 +1100

    4.3 created orders controller, views for orders and configured routs

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,8 @@ Rails.application.routes.draw do
   get 'static_pages/contact'
 
   get 'static_pages/index'
-  root 'static_pages#index'
+  root 'static_pages#landing_page'
+  resources :orders, only: [:index, :show, :create, :destroy]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
