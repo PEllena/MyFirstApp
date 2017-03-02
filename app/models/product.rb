8767c8532ff780657bee82e03e3d2224a480f084
Elena Panfilova <ellena.panfilova@gmail.com>
2017-03-02 23:34:13 +1100

    5.7 Generate a comment model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,7 @@
 class Product < ApplicationRecord
   has_many :orders
+  has_many :comments
+
   def self.search(search_term)
   	if Rails.env.development?
       @products = Product.where("name LIKE?", "%#{search_term}%")
@@ -7,5 +9,13 @@ class Product < ApplicationRecord
       @products = Product.where("name ilike?", "%#{search_term}%")
     end
   end
+
+  def highest_rating_comment
+	  comments.rating_desc.first
+	end
+
+  def lowest_rating_comment
+  	comments.rating_asc.first
+	end
 end
 
