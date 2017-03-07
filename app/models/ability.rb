a513f525c713b6577052a4dc2bdd82f873ebf667
Elena Panfilova <ellena.panfilova@gmail.com>
2017-03-08 00:55:22 +1100

    5.10 add admin role to users controller and admin moderation (delete) functionality.

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,6 +4,11 @@ class Ability
   def initialize(user)
     user ||= User.new # guest user (not logged in)
     can :manage, User, id: user.id
+    if user.admin?
+      can :manage, :all
+    else
+      can :read, :all
+    end
   end
 end
 
