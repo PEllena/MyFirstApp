cefa927f9dc1cdc3957b438db9ee9307c60d403f
Elena Panfilova <ellena.panfilova@gmail.com>
2017-03-19 18:01:25 +1100

    6.8 Install Brakeman security scanner and set session expiry time.

--- Gemfile
+++ Gemfile
@@ -47,6 +47,8 @@ gem 'rails-controller-testing'
 
 gem 'stripe'
 
+gem "brakeman", :require => false
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platform: :mri
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_nameofapp_session'
+Rails.application.config.session_store :cookie_store, key: '_nameofapp_session', :expire_after => 10.minutes
