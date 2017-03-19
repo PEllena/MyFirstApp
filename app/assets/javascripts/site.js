ddb7c77ab465e1d600d3de1bc7889df99c31f3ad
Elena Panfilova <ellena.panfilova@gmail.com>
2017-03-19 22:53:15 +1100

    6.9 Implement jQuery zoom plagin elevateZoom on product's show page.

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -1,13 +1,14 @@
 $(document).on('turbolinks:load', function(){
-    $('.rating').raty( { path: '/assets', scoreName: 'comment[rating]' });
-});
+	$('.rating').raty( { path: '/assets', scoreName: 'comment[rating]' });
+	$('.rated').raty({ path: '/assets',
+	  readOnly: true,
+	  score: function() {
+	    return $(this).attr('data-score');
+	  }
+	});
 
-$(document).on('turbolinks:load', function(){
-    $('.rating').raty( { path: '/assets', scoreName: 'comment[rating]' });
-    $('.rated').raty({ path: '/assets',
-      readOnly: true,
-      score: function() {
-        return $(this).attr('data-score');
-      }
-    });
+	$('.img-zoom').elevateZoom({
+		zoomType				: "inner",
+		cursor: "crosshair"
+	});
 });
