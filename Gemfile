0057a759818edcb8f408c5c2fe6cbaaa2e19f0df
Elena Panfilova <ellena.panfilova@gmail.com>
2017-03-21 15:01:26 +1100

    6.11 Use Russian Doll caching and implement Memcached in conjunction with Dalli gem.

--- Gemfile
+++ Gemfile
@@ -49,6 +49,9 @@ gem 'stripe'
 
 gem "brakeman", :require => false
 
+# client for accessing memcached servers
+gem 'dalli'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platform: :mri
--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,2 +1,8 @@
 module ProductsHelper
+	# This custom helper method checks the amount of products and whether one has been updated
+	def cache_key_for_products
+    count          = Product.count
+    max_updated_at = Product.maximum(:updated_at).try(:utc).try(:to_s, :number)
+    "products/all-#{count}-#{max_updated_at}"
+  end
 end
